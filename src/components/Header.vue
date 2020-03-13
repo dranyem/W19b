e0639e2b5da3bba1f7e927df47172c9bf1650b17
dranyem <davidmeynard@yahoo.com.ph>
2020-03-13 01:50:50 +0000

    Worked on the header of the page

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="header">
+        <h1>Header</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+    #header{
+        width: 100%;
+        height: 20vh;
+        background: lightblue;
+    }
+</style>
