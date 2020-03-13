559e6e994810a120c622e6535b1211a067f70325
dranyem <davidmeynard@yahoo.com.ph>
2020-03-13 02:04:51 +0000

    added files needed for the assignment

---
+++ src/components/Body.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="body">
+        <component1/>
+        <component2/>
+        <component3/>
+    </div>
+</template>
+
+<script>
+import component1 from './Component1.vue'
+import component2 from './Component2.vue'
+import component3 from './Component3.vue'
+export default {
+    name: "Body",
+    components:{
+        component1,
+        component2,
+        component3
+    }
+}
+</script>
+
+<style>
+    #body{
+        width:100%;
+        height: 60vh;
+        background: yellow;
+    }
+</style>
