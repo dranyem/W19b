f7f805f19a23df3feb6c82d2d74815ffe1f53ccf
dranyem <davidmeynard@yahoo.com.ph>
2020-03-13 02:09:04 +0000

    Worked on the footer part of the page

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="footer">
+        <h1>Footer</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Footer"
+}
+</script>
+
+<style scoped>
+    #footer{
+        width: 100%;
+        height: 15vh;
+        background: lightgreen;
+    }
+</style>
